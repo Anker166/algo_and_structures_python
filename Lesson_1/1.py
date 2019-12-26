89bca1b0040c239cb376c176e8b46faf2f26869f
Anker166 <andrei.k.166@mail.ru>
2019-12-26 17:13:15 +0500

    Выполнил 1 задание

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,1 +1,12 @@
-# 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+"""
+1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+"""
+
+NUM = int(input('Введите трехзначное число:\n'))
+FIRST_DIGIT = NUM // 100
+SECOND_DIGIT = NUM % 100 // 10
+THIRD_DIGIT = NUM % 10
+DIGITS_SUM = FIRST_DIGIT + SECOND_DIGIT + THIRD_DIGIT
+print(f"Сумма цифр введенного числа:\n{DIGITS_SUM}")
+DIGITS_MULTIPLICATION = FIRST_DIGIT * SECOND_DIGIT * THIRD_DIGIT
+print(f"Произведение цифр введенного числа:\n{DIGITS_MULTIPLICATION}")
