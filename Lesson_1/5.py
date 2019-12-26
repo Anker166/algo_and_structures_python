81f406e1aa7d4cad144e5bf371a5b93516e7762c
Anker166 <andrei.k.166@mail.ru>
2019-12-26 17:15:56 +0500

    Выполнил 5 задание, использовал функции chr и ord

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,13 @@
-#5.	Пользователь вводит две буквы. Определить, на каких местах
-# алфавита они стоят, и сколько между ними находится букв.
+"""
+5.	Пользователь вводит две буквы. Определить, на каких местах
+алфавита они стоят, и сколько между ними находится букв.
+"""
+
+FIRST_LETTER = input('Введите первую букву:\n').lower()
+SECOND_LETTER = input('Введите вторую букву:\n').lower()
+FIRST_POSITION = ord(FIRST_LETTER) - 97 + 1
+print(f'Буква "{FIRST_LETTER}" находится на {FIRST_POSITION} месте')
+SECOND_POSITION = ord(SECOND_LETTER) - 97 + 1
+print(f'Буква "{SECOND_LETTER}" находится на {SECOND_POSITION} месте')
+LETTERS_NUMBER = abs(ord(SECOND_LETTER) - ord(FIRST_LETTER)) - 1
+print(f'Между "{FIRST_LETTER}" и "{SECOND_LETTER}" находится {LETTERS_NUMBER} букв')
