56e08a6e419bf3d38ad172fc0ba5ea26a3060711
Anker166 <andrei.k.166@mail.ru>
2019-12-26 17:17:03 +0500

    Выполнил 9 задание

--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -1,3 +1,14 @@
-# 9.Вводятся три разных числа. Найти, какое из них
-# является средним (больше одного, но меньше другого).
+"""
+9.Вводятся три разных числа. Найти, какое из них
+является средним (больше одного, но меньше другого).
+"""
+FIRST_NUM = int(input('Введите первое число:\n'))
+SECOND_NUM = int(input('Введите второе число:\n'))
+THIRD_NUM = int(input('Введите третье число:\n'))
 
+if (SECOND_NUM < FIRST_NUM < THIRD_NUM) or (THIRD_NUM < FIRST_NUM < SECOND_NUM):
+    print(f'Средним является число: {FIRST_NUM}')
+elif (FIRST_NUM < SECOND_NUM < THIRD_NUM) or (THIRD_NUM < SECOND_NUM < FIRST_NUM):
+    print(f'Средним является число: {SECOND_NUM}')
+else:
+    print(f'Средним является число: {THIRD_NUM}')
