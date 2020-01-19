65f6d449863a3fff9857d5c2fd787dddfe7e48bc
Anker166 <andrei.k.166@mail.ru>
2020-01-19 19:26:52 +0300

    Выполнил 4 задание

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,16 @@
-# 4.	Определить, какое число в массиве встречается чаще всего.
+"""
+4.	Определить, какое число в массиве встречается чаще всего.
+"""
+
+from random import randint
+
+TASK_LIST = [randint(0, 100) for i in range(30)]
+TASK_ELEM = 0
+COUNT_ELEM = 0
+for elem in TASK_LIST:
+    if TASK_LIST.count(elem) > COUNT_ELEM:
+        TASK_ELEM = elem
+        COUNT_ELEM = TASK_LIST.count(elem)
+print(f'Исходный массив: {TASK_LIST}\n'
+      f'Число {TASK_ELEM} встречается {COUNT_ELEM} раз'
+      )
