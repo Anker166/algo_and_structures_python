b9bafd87334bcbfdd581ed4c3e3b451a137a1362
Anker166 <andrei.k.166@mail.ru>
2020-01-19 19:27:01 +0300

    Выполнил 6 задание

--- Lesson_3/6.py
+++ Lesson_3/6.py
@@ -3,3 +3,18 @@
 между минимальным и максимальным элементами.
 Сами минимальный и максимальный элементы в сумму не включать.
 """
+from random import randint
+
+TASK_LIST = [randint(-100, 100) for i in range(20)]
+MAX_ELEM = max(TASK_LIST)
+MIN_ELEM = min(TASK_LIST)
+MAX_IND = TASK_LIST.index(MAX_ELEM)
+MIN_IND = TASK_LIST.index(MIN_ELEM)
+if MAX_IND > MIN_IND:
+    ELEM_SUM = sum(TASK_LIST[MIN_IND + 1:MAX_IND])
+else:
+    ELEM_SUM = sum(TASK_LIST[MAX_IND + 1:MIN_IND])
+print(f'Исходный массив: {TASK_LIST}\n'
+      f'Максимальный элемент: {MAX_ELEM}, Индекс: {MAX_IND}\n'
+      f'Мигимальный элемент: {MIN_ELEM}, Индекс: {MIN_IND}\n'
+      f'Сумма: {ELEM_SUM}')
