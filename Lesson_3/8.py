904d107292ef9ed29c4a9d74557e79b8224671d5
Anker166 <andrei.k.166@mail.ru>
2020-01-19 19:27:10 +0300

    Выполнил 8 задание

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,15 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+
+ROWS = 5
+COLUMNS = 4
+TASK_MATRIX = []
+for i in range(ROWS):
+    USER_INPUT = input(f'Введите {COLUMNS - 1} числа через пробел '
+                       f'для {i + 1} строки:\n').split()
+    USER_ROW = [int(i) for i in USER_INPUT]
+    USER_ROW.append(sum(USER_ROW))
+    TASK_MATRIX.append(USER_ROW)
+for row in TASK_MATRIX:
+    print(row)
