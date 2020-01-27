4ac5c5a35fcd314ca218ce4b6b7f7ef79681fd6e
Anker166 <andrei.k.166@mail.ru>
2020-01-27 12:31:12 +0300

    Выполнил 2 задание

--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,17 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+from collections import deque
+
+
+FIRST_NUM = deque(input('Введите первое число:\n').upper())
+SECOND_NUM = deque(input('Введите второе число:\n').upper())
+
+FIRST_HEX = ''.join(i for i in FIRST_NUM)
+SECOND_HEX = ''.join(i for i in SECOND_NUM)
+
+HEX_SUM = hex(int(FIRST_HEX, 16) + int(SECOND_HEX, 16))
+print(f'Сумма чисел равна: {HEX_SUM[2:].upper()}')
+
+HEX_MUL = hex(int(FIRST_HEX, 16) * int(SECOND_HEX, 16))
+print(f'Произведение чисел равна: {HEX_MUL[2:].upper()}')
